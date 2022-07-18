097400357c23406d65b46a95b8dcbc56383e3725
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2022-07-18 11:23:54 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./index.css";
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Flash Sale !</h1>
+        <div className="card">ok</div>
+        <div className="card">ok</div>
+        <div className="card">ok</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
